09cf4bd177d6a74150c54af7ce64096284f4b43b
anjiao <anjiaoyzyz@163.com>
2018-07-24 17:17:05 +0800

    定义自定义控件

---
+++ tabmenu/src/main/java/com/joe/tabmenu/IconWithTextMenu.java
@@ -0,0 +1,20 @@
+package com.joe.tabmenu;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by joe.
+ */
+public class IconWithTextMenu extends LinearLayout {
+
+    public IconWithTextMenu(Context context) {
+        super(context);
+    }
+
+    public IconWithTextMenu(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
